186d2dd6ca7232af72e7306e43c5b0fe483e458b
Daniela <danamattl@yahoo.com>
2025-01-04 05:30:42 +0100

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation  } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import HeaderDM from './components/Header/HeaderDM';
 import Footer from './components/Foooter/Footer';
 import Apropos from './pages/Apropos/Apropos';
 import './App.css'
 
-function FindHash() {
-  const location = useLocation();
-  useEffect(() => {
-    if (location.hash) {
-      const element = document.querySelector(location.hash);
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
-  }, [location]);
-
-  return null;
-}
-
 function App() {
   return (
-    <Router>
+    <Router basename="/my_portfolio">
       <HeaderDM />
       <Routes> 
         <Route path="/" element={<Home />} />
