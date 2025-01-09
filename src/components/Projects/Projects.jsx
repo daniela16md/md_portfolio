b4e8ba39abbf2bfe1266418b6778c44158bfe562
Daniela <danamattl@yahoo.com>
2025-01-09 17:14:40 +0100

    animations

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FaGithub, FaLink } from 'react-icons/fa';
-import ReactModal from 'react-modal'; 
+import ReactModal from 'react-modal';
 import projects from '../../Data/projets.json'; 
 import './Projects.css';
 
@@ -8,15 +8,16 @@ ReactModal.setAppElement('#root');
 
 function Projects() {
   const [selectedTech, setSelectedTech] = useState('');
-  const [isModalOpen, setIsModalOpen] = useState(false); 
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentUrl, setCurrentUrl] = useState('');
   const [showProjects, setShowProjects] = useState(false);
   const [activeProject, setActiveProject] = useState(null);
+  const [projectList, setProjectList] = useState(projects.projects); 
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowProjects(true); 
-    }, 1000); 
+    }, 1000);
     return () => clearTimeout(timer);
   }, []);
 
@@ -25,8 +26,8 @@ function Projects() {
   };
 
   const filteredProjects = selectedTech
-    ? projects.projects.filter(project => project.technologies.includes(selectedTech))
-    : projects.projects;
+    ? projectList.filter(project => project.technologies.includes(selectedTech))
+    : projectList;
 
   const techOptions = ['HTML', 'CSS', 'JS', 'React', 'NodeJS', 'Redux', 'SEO', 'Lighthouse', 'SCSS'];
 
@@ -39,19 +40,46 @@ function Projects() {
     setIsModalOpen(false);
     setCurrentUrl('');
   };
+
   const handleProjectClick = (project) => {
     setActiveProject(project);
   };
+
   const closeProjectDetails = () => {
     setActiveProject(null);
-  }
+  };
+
+  const handleDragStart = (event, projectId) => {
+    event.dataTransfer.setData('projectId', projectId);
+  };
+
+  const handleDrop = (event, targetProjectId) => {
+    event.preventDefault();
+    const draggedProjectId = event.dataTransfer.getData('projectId');
+    const draggedProjectIndex = projectList.findIndex(project => project.id === parseInt(draggedProjectId));
+    const targetProjectIndex = projectList.findIndex(project => project.id === parseInt(targetProjectId));
+
+    if (draggedProjectIndex !== -1 && targetProjectIndex !== -1) {
+      const updatedProjects = [...projectList];
+      const temp = updatedProjects[draggedProjectIndex];
+      updatedProjects[draggedProjectIndex] = updatedProjects[targetProjectIndex];
+      updatedProjects[targetProjectIndex] = temp;
+      setProjectList(updatedProjects);
+      
+    }
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault(); 
+  };
 
   return (
-    <div className="projects-page">
-      <h2 className='sectionh2'>Mes Projets</h2>
+    <div className="projects-page" onDrop={(event) => handleDrop(event, null)} onDragOver={handleDragOver}>
+      <h2 className="sectionh2">Mes Projets</h2>
+
       <div className="filters">
-        <label htmlFor="techFilter" className='labelfiltre'>Filtrer par technologie</label>
-        <select id="techFilter" value={selectedTech} onChange={handleTechChange} aria-label="Filtrer par technologie" >
+        <label htmlFor="techFilter" className="labelfiltre">Filtrer par technologie</label>
+        <select id="techFilter" value={selectedTech} onChange={handleTechChange} aria-label="Filtrer par technologie">
           <option value="">Tous les projets</option>
           {techOptions.map((tech, index) => (
             <option key={index} value={tech}>{tech}</option>
@@ -66,41 +94,54 @@ function Projects() {
               key={project.id}
               className="project-card"
               style={{
-                animationDelay: `${(index + 1) * 1}s`, 
+                animationDelay: `${(index + 1) * 1}s`,
               }}
+              draggable="true"
+              onDragStart={(event) => handleDragStart(event, project.id)}
+              onDrop={(event) => handleDrop(event, project.id)}
+              onDragOver={handleDragOver}
             >
-               {activeProject && activeProject.id === project.id ? ( 
+              {activeProject && activeProject.id === project.id ? (
                 <div className="project-details" onClick={closeProjectDetails}>
                   <p><strong>Technologies:</strong> {project.technologies}</p>
                   <p>{project.text}</p>
-                  <div className='lienssite'>
+                  <div className="lienssite">
                     {project.github && (
-                      <button className="button-project-link" onClick={() => window.open(project.github, '_blank')} aria-label={`Voir le projet sur GitHub: ${project.name}`}>
-                        <FaGithub /><span className="button-text">Le repository Github</span>
+                      <button
+                        className="button-project-link"
+                        onClick={() => window.open(project.github, '_blank')}
+                        aria-label={`Voir le projet sur GitHub: ${project.name}`}
+                      >
+                        <FaGithub />
+                        <span className="button-text">Le repository Github</span>
                       </button>
                     )}
                     {project.lien && (
-                      <button className="button-project-link" onClick={() => openModal(project.lien)} aria-label={`Voir le lien du projet: ${project.name}`}>
-                        <FaLink /> <span className="button-text">Le lien vers le site</span>
+                      <button
+                        className="button-project-link"
+                        onClick={() => openModal(project.lien)}
+                        aria-label={`Voir le lien du projet: ${project.name}`}
+                      >
+                        <FaLink />
+                        <span className="button-text">Le lien vers le site</span>
                       </button>
                     )}
                   </div>
                 </div>
               ) : (
-               <div onClick={() => handleProjectClick(project)}>
-                <img
-                  src={project.image}
-                  alt={`Image du projet ${project.name}`}
-                  className="project-image"
-                  loading="lazy"
-                  width="300"
-                  height="150"
-                  onClick={() => handleProjectClick(project.id)} 
-                />
-                <h3>{project.name}</h3>
-               </div>
+                <div onClick={() => handleProjectClick(project)}>
+                  <img
+                    src={project.image}
+                    alt={`Image du projet ${project.name}`}
+                    className="project-image"
+                    loading="lazy"
+                    width="300"
+                    height="150"
+                  />
+                  <h3>{project.name}</h3>
+                </div>
               )}
-              </div>
+            </div>
           ))
         ) : (
           <p>Aucun projet trouvé pour cette technologie.</p>
@@ -109,11 +150,11 @@ function Projects() {
 
       <ReactModal
         isOpen={isModalOpen}
-        onRequestClose={closeModal} 
+        onRequestClose={closeModal}
         contentLabel="Project Link"
         className="modal-content modal-content-projets"
         overlayClassName="modal-overlay"
-        ariaHideApp={false} 
+        ariaHideApp={false}
       >
         <button className="modal-close" onClick={closeModal}>X</button>
         <iframe src={currentUrl} width="100%" height="600px" title="Project Site" />
--- src/components/Skills/Skllls.jsx
+++ src/components/Skills/Skllls.jsx
@@ -1,43 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { FaHtml5, FaCss3Alt, FaSass, FaJsSquare, FaReact, FaCodeBranch, FaBug, FaSearchengin, FaLanguage, FaGithub,} from 'react-icons/fa';
+import { FaHtml5, FaCss3Alt, FaSass, FaJsSquare, FaReact, FaCodeBranch, FaBug, FaSearchengin, FaLanguage, FaGithub } from 'react-icons/fa';
 import { SiSwagger } from "react-icons/si";
 import { IoLogoNodejs } from 'react-icons/io';
 import { FaCogs } from 'react-icons/fa';
+import { FaRandom } from 'react-icons/fa';
 import skillsData from '../../Data/softskills.json';
-import './Skills.css'
+import './Skills.css';
+
 const Skills = () => {
   const [skills, setSkills] = useState([]);
 
   useEffect(() => {
     setSkills(skillsData.skillsData);
   }, []);
-
-  const handleDragStart = (event, skillId) => {
-    event.dataTransfer.setData('skillId', skillId);
-  };
-
-  const handleDrop = (event, targetSkillId) => {
-    event.preventDefault();
-    const draggedSkillId = event.dataTransfer.getData('skillId');   
-    const draggedSkillIndex = skills.findIndex(skill => skill.id === parseInt(draggedSkillId));
-    const targetSkillIndex = skills.findIndex(skill => skill.id === parseInt(targetSkillId));
-
-    if (draggedSkillIndex !== -1 && targetSkillIndex !== -1) {
-      const updatedSkills = [...skills];
-      const temp = updatedSkills[draggedSkillIndex];
-      updatedSkills[draggedSkillIndex] = updatedSkills[targetSkillIndex];
-      updatedSkills[targetSkillIndex] = temp;
-      setSkills(updatedSkills);
+  const shuffleSkills = () => {
+    const shuffledSkills = [...skills];
+    for (let i = shuffledSkills.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledSkills[i], shuffledSkills[j]] = [shuffledSkills[j], shuffledSkills[i]]; 
     }
-  };
-
-  const handleDragOver = (event) => {
-    event.preventDefault();  
+    setSkills(shuffledSkills); 
   };
 
   return (
-    <div className="softskills-container" onDrop={(event) => handleDrop(event, null)} onDragOver={handleDragOver}>
-      <h2 className='sectionh2'> Skills</h2>
+    <div className="softskills-container">
+      <h2 className="sectionh2">Skills</h2>
+      <button className="shuffle-button" onClick={shuffleSkills}>Réorganiser les compétences <FaRandom  /></button>
       <div className="skills-list">
         {skills.length > 0 ? (
           skills.map((skill) => {
@@ -53,21 +41,22 @@ const Skills = () => {
                         skill.icon === 'FaGithub' ? FaGithub :
                         skill.icon === 'SiSwagger' ? SiSwagger :
                         skill.icon === 'IoLogoNodejs' ? IoLogoNodejs :
-                        skill.icon === ' FaCogs' ?  FaCogs :
+                        skill.icon === 'FaCogs' ? FaCogs :
                         null;
 
             return (
-              <div key={skill.id} className="skill-card" draggable="true" onDragStart={(event) => handleDragStart(event, skill.id)}
-                onDrop={(event) => handleDrop(event, skill.id)}
-                onDragOver={handleDragOver}
-                >
+              <div key={skill.id} className="skill-card">
+              {Icon ? (
                 <Icon className="skill-icon" style={{ color: skill.color }} />
-                <p className="skill-name" >{skill.name}</p>
-              </div>
+              ) : (
+                <p>Icône introuvable</p>  
+              )}
+              <p className="skill-name">{skill.name}</p>
+            </div>
             );
           })
         ) : (
-          <p>Chargement...</p>  
+          <p>Chargement...</p>
         )}
       </div>
     </div>
